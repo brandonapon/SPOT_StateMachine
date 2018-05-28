3cdaad4930407add30e688750698fb244cfa2b91
Brandon <bapon@umail.ucsb.edu>
2018-05-28 00:10:08 -0700

    Reformated folder
    unzipped and replaced files

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
 from state_machine import Device
+from state import State
+from spot_states import *
+from ui import *
+import asyncio
+import time
 
-"""
-Unclear:
+def __init__(self)
+    self.iop = pynq.lib.PynqMicroblaze(mb_info, IOP_EXECUTABLE)
+    
+class SPOT(object):
+    """
+    Object declaration for SPOT device.
+    """
 
-1) How to designate BOOT UP signal?
-"""
+    def __init__(self, user):
+        """
+        Initialize all components for SPOT device.
+        """
+        self.state = MainState()
+        self.user = user
+    
+    def on_event(self, event):
+        """
+        This is the main chunk of the state machine. Incoming events are delegated to 
+        the given states which then handle the event. The result is then assigned as
+        the new state.
+        """
 
+        # Next state will be the result of of the on_event function.
+        self.state = self.state.on_event(event)
+
+async def button_to_led(number):
+    button = base.buttons[number]
+    led = base.leds[number]
+    while True:
+        await button.wait_for_level_async(1)
+        led.on()
+        await button.wait_for_level_async(0)
+        led.off()
+
+
+async def wake_up(delay):
+    '''A coroutine that will yield to asyncio.sleep() for a few seconds
+       and then resume, having preserved its state while suspended
+    '''
+
+    start_time = time.time()
+    print(f'The time is: {time.strftime("%I:%M:%S")}')
+    print(f"Suspending coroutine 'wake_up' at 'await` statement\n")
+    await asyncio.sleep(delay)
+    print(f"Resuming coroutine 'wake_up' from 'await` statement")
+    end_time = time.time()
+    sleep_time = end_time - start_time
+    print(f"'wake-up' was suspended for precisely: {sleep_time} seconds")
 
 # This is the MAIN (grand/event) LOOP.
 def main():
     # Instantiate SPOT device. Initial state is "Main".
-    spot = Device("SPOT")
+    # This object is the PHYSICAL (as in, peripherals) device.
+    # NOT THE USER INTERFACE!
+    device = SPOT("TEST_USER")
 
     # Create UI object.
     interface = UI()
@@ -19,92 +68,28 @@ def main():
     button_pair = ButtonPair()
     button_pair.configuration_1()
 
-    # Draw the Main screen
-    interface.drawMainScreen()
-
-
     """
-    Need a SWITCH CASE statement
-    Copy paste the code below into the switch-case
-    State machine will adapt automatically
+    1) Either need to link button pair to SPOT device, OR
+    2) Make button object part of SPOT physical device (above).
+
+    The issue here is that the buttons are the "glue" between the 
+    interface and the physical device. Probably better to keep it 
+    a separate object and link them manually.
     """
 
-    if button_1.pressed == True: # VIEW is pressed
-        spot.trigger("Main->View")
-        interface.drawViewScreen() # Main->View
-        button_pair.configuration_2()
-
-        if button_1.pressed == True: # CANCEL is pressed
-            spot.trigger("Main->View->Cancel")
-            interface.drawMainScreen() # Main
-            button_pair.configuration_1()
-        elif button_2.pressed == True: # SELECT is pressed
-            spot.trigger("Main->View->Select")
-            interface.drawSelectView() # Main->View->Selected
-            button_pair.configuration_3()
-
-            if button_1.pressed == True: # HOME is pressed
-                spot.trigger("Main->View->Select->Home")
-                interface.drawMainScreen() # Main
-                button_pair.configuration_1()
-            elif button_2.pressed == True: # GO BACK is pressed
-                spot.trigger("Main->View->Select->Go Back")
-                interface.drawSelectView() # Main->View->Selected
-                button_pair.configuration_3()
-
-    elif button_2.pressed == True: # MARK is pressed
-        spot.trigger("Main->Mark")
-        interface.drawMarkScreen() # Main->Mark
-        button_pair.configuration_4()
-
-        if button_1.pressed == True: # CANCEL is pressed
-            spot.trigger("Main->Mark->Cancel")
-            interface.drawMainScreen() # Main
-            button_pair.configuration_1()
-        elif button_2.pressed == True: # MARK is pressed
-            spot.trigger("Main->Mark->Mark")
-            interface.drawAfterMark() # Mark->Mark->Mark
-            button_pair.configuration_5()
-
-            if button_1.pressed == True: # CANCEL is PRESSED
-                spot.trigger("Main->Mark->Mark->Cancel")
-                interface.drawMarkScreen() # Main->Mark
-                button_pair.configuration_4()
-            elif button_2.pressed == True: # CONFIRM is pressed
-                spot.trigger("Main->Mark->Mark->Confirm")
-                interface.drawMainScreen() # Main
-                button_pair.configuration_1()
+    radar = Radar()
 
 
-''' Alert Screens ?????? '''
-# Need some way of signaling that there is an ALERT
-if ALERT_INTEREST == True: # <----- NEED TO CHANGE
-    interface.drawAlertInterest() # Alert (Interest)
-    button_pair.configuration_6()
-
-    if button_1.pressed == True: # DISMISS is pressed
-        spot.trigger("AlertInt->Dismiss")
-        interface.drawMainScreen() # Main
-        button_pair.configuration_1()
-    elif button_2.pressed == True: # VIEW is pressed
-        spot.trigger("AlertInt->View")
-        interface.drawSelectView() # Main->View->Selected
-        button_pair.configuration_3()
-
-
-if ALERT_DANGER == True: # <----- NEED TO CHANGE
-    interface.drawAlertDanger() # Alert (Danger)
-    button_pair.configuration_6()
-
-    if button_1.pressed == True: # DISMISS is pressed
-        spot.trigger("AlertDan->Dismiss")
-        interface.drawMainScreen() # Main
-        button_pair.configuration_1()
-    elif button_2.pressed == True: # VIEW is pressed
-    spot.trigger("AlertDan->View")
-        interface.drawSelectView() # Main->View->Selected
-        button_pair.configuration_3()
-
-
+"""
+NEED A WAY TO TRIGGER A KERNEL RESTART 
+Just the reset button on the PYNQ? Or power cycle?
+"""
 if __name__ == '__main__':
-    main()
+    eventLoop = asyncio.get_event_loop()
+    try:
+        print("Creating task for coroutine 'main'\n")
+        wakeUpTask = eventLoop.create_task()
+    except RuntimeError as error:
+        print(f'{error}' + ' - restart kernel to re-run the event loop')
+    finally:
+        eventLoop.close()
--- spot_states.py
+++ spot_states.py
@@ -1,30 +1,184 @@
 from state import State
+from ui import UI
+import time
+import datetime
+
+# relevant events for now: TOP and BOTTOM
+# error event is ERROR
+
+haptic = Haptic() # Declare haptic motor here?
+rangefinder = Rangefinder() # Declare rangefinder here?
+camera = Camera() # Declare camera here?
+# Might need to create different temp objects for each peripheral. Easier to store data that way.
+# Could maybe get away with using the asyncio calls only, but could be issues w/ timing.
+
+global counter = 1
+
+def createTimestamp():
+	timestamp = time.time()
+	timestamp_string_converted = datetime.datetime.fromtimestamp(timestamp).strftime('%Y%m%d_%H%M')
+	return timestamp_string_converted
 
 class MainState(State):
 	"""
 	State that shows MAIN screen.
 	"""
+	
+	self.state = 'MAIN'
+	self.button_pressed = False
+	self.button = ''
+
+	def __init__(self):
+		interface.drawMainScreen() # Main
+    	button_pair.configuration_1()
+
+	def assignButton(self):
+		# check what button is pressed, and assign respective string
+		if self.TOP_BUTTON:
+
 	def on_event(self, event):
-		if event == 'MAIN':
-			return 
-
-
-class InfoState(State):
-	pass
-
+		if event == 'TOP':
+			return ViewState()
+		elif event == 'BOTTOM':
+			return MarkState()
+		return self
+	
+	def loop(self):
+		while True:
+			try:
+				print("Currently in <MAIN> state")
+				# code for new state change
+			except ValueError:
+				print("ERROR: Cannot access from current state")
 
 class ViewState(State):
-	pass
+	"""
+	State that shows VIEW screen.
+	"""
+	def __init__(self):
+		interface.drawViewScreen() # Main
+    	button_pair.configuration_2()
 
+	def on_event(self, event):
+		if event == 'TOP':
+			return MainState()
+		elif event == 'BOTTOM':
+			return ConfirmState()
+		return self
+
+	def loop(self):
+		while True:
+			try:
+				print("Currently in <MAIN> state")
+				# code for new state change
+			except ValueError:
+				print("ERROR: Cannot access from current state")
+
+class InfoState(State):
+	"""
+	State that shows INFO screen.
+	"""
+	def __init__(self):
+		interface.drawSelectView()
+    	button_pair.configuration_3()
+
+	def on_event(self, event):
+		if event == 'TOP':
+			return MainState()
+		elif event == 'BOTTOM':
+			return ViewState()
+		return self
+
+	def loop(self):
+		while True:
+			try:
+				print("Currently in <MAIN> state")
+				# code for new state change
+			except ValueError:
+				print("ERROR: Cannot access from current state")
 
 class MarkState(State):
-	pass
+	"""
+	State that shows MARK screen.
 
+	THIS IS THE ONLY FUNCTION WHERE WE NEED:
+	1) RANGEFINDER
+	2) CAMERA
+	"""
+	def __init__(self):
+		interface.drawMarkScreen()
+    	button_pair.configuration_4()
+
+	def on_event(self, event):
+		if event == 'TOP':
+			return MainState()
+		elif event == 'BOTTOM':
+			return ConfirmState()
+		return self
+
+	def loop(self):
+		while True:
+			try:
+				print("Currently in <MAIN> state")
+				# code for new state change
+			except ValueError:
+				print("ERROR: Cannot access from current state")
 
 class ConfirmState(State):
-	pass
+	"""
+	State that shows CONFIRM MARK screen.
+	"""
 
+	global counter
+	self.point = Point()
+
+	def __init__(self):
+		self.point.distance = rangefinder.poll() # <--------- Placeholder
+		self.point.picture = camera.snapshot() # <-------- Placeholder, I forgot the exact command despite having done it a million times...
+		interface.drawAfterMark()
+    	button_pair.configuration_5()
+
+	def on_event(self, event):
+		if event == 'TOP':
+			return MarkState()
+		elif event == 'BOTTOM':
+			stamp = createTimestamp()
+			self.point.name = 'point_{}'.format(stamp)
+			# self.point.name = 'point_{}'.format(counter)
+			global counter = counter + 1
+			return MainState()
+		return self
+
+	def loop(self):
+		while True:
+			try:
+				print("Currently in <MAIN> state")
+				# code for new state change
+			except ValueError:
+				print("ERROR: Cannot access from current state")
 
 class AlertState(State):
-	pass
+	"""
+	State that shows ALERT screen.
 
+	----> NEED HAPTIC IN THIS STATE <----
+	"""
+	def __init__(self):
+		haptic.buzz()
+		interface.drawAlertInterest() # Main
+    	button_pair.configuration_6()
+
+	def on_event(self, event):
+		if event == 'TOP':
+			return MainState()
+		elif event == 'BOTTOM':
+			return InfoState()
+		return self
+
+	def loop(self):
+		while True:
+			try:
+				print("Currently in <MAIN> state")
+				# code for new state change
+			except ValueError:
+				print("ERROR: Cannot access from current state")
